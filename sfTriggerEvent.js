4d632bb9eee4746673e7f2ec511ca68b668072b8
Srikanth Kuruva <srikanth.kuruva@srikanthku-ltm.internal.salesforce.com>
2021-08-31 10:23:04 +1000

    working copy: successful response from publish event api

--- sfTriggerEvent.js
+++
@@ -1,8 +0,0 @@
-import {getAccessToken} from './sfAuth.js'
-
-getAccessToken()
-.then( oAuthResponse => {
-    console.log('oAuthResponse::');
-    console.log(oAuthResponse);
-})
-.catch(error =>{console.log(error)});
